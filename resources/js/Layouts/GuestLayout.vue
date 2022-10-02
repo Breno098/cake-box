b94162536d64d1aaf8c2104962125a0907a3fdda
Breno <breno@agily.com.br>
2022-10-02 20:50:30 -0300

    BootStrap, Blezze and Inertia

---
+++ resources/js/Layouts/GuestLayout.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+    <div class="container">
+        <div class="d-flex justify-content-center align-items-center vh-100">
+            <slot />
+        </div>
+    </div>
+</template>
---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,78 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
+
+defineProps({
+    canResetPassword: Boolean,
+    status: String,
+});
+
+const form = useForm({
+    email: '',
+    password: '',
+    remember: false
+});
+
+const submit = () => {
+    form.post(route('login'), {
+        onFinish: () => form.reset('password'),
+    });
+};
+</script>
+
+<template>
+    <GuestLayout>
+        <Head title="Log in" />
+
+        <!-- <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+            {{ status }}
+        </div> -->
+
+        <form
+            class="text-center w-50"
+            @submit.prevent="submit"
+        >
+            <i class="bi bi-person-workspace" style="font-size: 3rem;"></i>
+
+            <div class="card">
+                <div class="card-header">
+                    Featured
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+
+            <!-- <div>
+                <InputLabel for="email" value="Email" />
+                <TextInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus autocomplete="username" />
+                <InputError class="mt-2" :message="form.errors.email" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="password" value="Password" />
+                <TextInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="current-password" />
+                <InputError class="mt-2" :message="form.errors.password" />
+            </div>
+
+            <div class="block mt-4">
+                <label class="flex items-center">
+                    <Checkbox name="remember" v-model:checked="form.remember" />
+                    <span class="ml-2 text-sm text-gray-600">Remember me</span>
+                </label>
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
+                    Forgot your password?
+                </Link>
+
+                <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                    Log in
+                </PrimaryButton>
+            </div> -->
+        </form>
+    </GuestLayout>
+</template>
